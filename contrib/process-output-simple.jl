c9a68e9090e5afd1cba902df1eb288b324033011
mschauer <moritzschauer@web.de>
2019-03-07 17:39:05 +0100

    Plotting in a function

--- contrib/process-output-simple.jl
+++ contrib/process-output-simple.jl
@@ -2,45 +2,43 @@ using DataFrames
 using Statistics
 using RCall
 
-p = .05
-observations = res.observations
-breaks = res.breaks
-title = res.title
-ψ = res.ψ
-N = res.N
-acc = res.acc
-λ = λinfo.λ
-T = res.T
 
-A = view(ψ, size(ψ, 1)÷2:size(ψ, 1), :)
-upper = mapslices(v-> quantile(v, 1 - p/2), A, dims=1)
-med = median(A, dims=1)
-ave = mean(A,dims=1)
-lower = mapslices(v-> quantile(v,  p/2), A, dims=1)
 
-## plotting
+function plotposterior(res; figtitle="Markov chain prior", offset=0, hortics=0)
+        ψ= res.ψ
+        p = 0.05
+        A = view(ψ, size(ψ, 1)÷2:size(ψ, 1), :)
+        upper = mapslices(v -> quantile(v, 1 - p/2), A, dims=1)
+        med = median(A, dims=1)
+        ave = mean(A, dims=1)
+        lower = mapslices(v -> quantile(v,  p/2), A, dims=1)
+        breaks = offset.+ res.breaks
+        N = length(breaks) - 1
 
-# dMarkov sets the bands of the posterior
-dMarkov = DataFrame(average=vec(ave), lower=vec(lower),
-                    median=vec(med), upper=vec(upper),
-                    xmin=breaks[1:N],xmax=breaks[2:end])
-# tMarkov sets the posterior mean (need to duplicate the final value)
-tMarkov = DataFrame(x=breaks, average=[vec(ave); vec(ave)[end]])
+        summaryψ=[ave' lower' med' upper']
+        # dMarkov sets the bands of the posterior
+        dMarkov = DataFrame(average=vec(ave), lower=vec(lower),
+                            median=vec(med),  upper=vec(upper),
+                            xmin=breaks[1:N], xmax=breaks[2:end])
 
-obs = DataFrame(x=observations)
+        # dTrue contains the true intensity, evaluated on a grid
+#        dTrue = DataFrame(x=gr, intensity=vals)
 
-@rlibrary ggplot2
-p = ggplot() + geom_rect(data=dMarkov,
-aes(xmin = :xmin, xmax = :xmax, ymin = :lower, ymax = :upper), fill = "lightsteelblue1") +
-geom_step(data=tMarkov, aes(x=:x, y=:average),colour="black",size=1.3)+
-ggtitle("Markov chain prior") +
-xlab("") + ylab("") +
-geom_rug(data=obs, mapping=aes(x=:x), color="black", sides="t")
+        # tMarkov sets the posterior mean (need to duplicate the final value)
+        tMarkov = DataFrame(x=breaks, average=[vec(ave); vec(ave)[end]])
+        obs = DataFrame(x=res.observations)
 
-if !ismissing(λ)
-    gr = range(0,stop=T,length=1000); vals = λ.(gr)
-    dTrue = DataFrame(x=gr, intensity=vals)
-    show(p + geom_line(data=dTrue, aes(x=:x,y=:intensity),colour="red",linetype = "solid",size=1.5) )
-else
-    show(p)
+        #@rlibrary ggplot2  # cannot be called within the function appararently
+        p = ggplot() + geom_rect(data=dMarkov,
+        aes(xmin=:xmin, xmax=:xmax, ymin = :lower, ymax = :upper), fill = "lightsteelblue1") +
+        geom_step(data=tMarkov, aes(x=:x, y=:average), colour="black", size=1.3) +
+        ggtitle(figtitle) +
+        xlab("") + ylab("") +
+        geom_rug(data=obs, mapping=aes(x=:x), color="black", sides="t")
+        if hortics == 0
+            pp = p+ scale_x_continuous(limits=[minimum(breaks), maximum(breaks)])
+        else
+            pp = p + scale_x_continuous(limits=[minimum(breaks), maximum(breaks)], breaks=hortics)
+        end
+        pp
 end
