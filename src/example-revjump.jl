d529baaa4ee667aca5cee1464ebe074403562c99
fmeulen <f.h.vandermeulen@tudelft.nl>
2019-11-15 21:20:37 +0100

    Update example-revjump.jl

--- src/example-revjump.jl
+++ src/example-revjump.jl
@@ -1,5 +1,6 @@
+ #add https://github.com/mschauer/PointProcessInference.jl#revjump
 using DelimitedFiles
-using PointProcessInference
+using PointProcessInference#revjump
 using Distributions
 using DataFrames
 using RCall
@@ -14,9 +15,9 @@ observations = vec(readdlm("testdat_n1.csv"))
 
 ########## run the rev jump algorithm
 α = 0.1; β=0.1
-Δvec, Hvec = computebinning(T; Nmax = 40)
-priorN = Poisson(23)
-ITER = 100
+Δvec, Hvec = computebinning(observations,T; Nmax = 40)
+priorN = DiscreteUniform(1,Nmax) #Poisson(23)
+ITER = 10000
 states, df = revjump(observations,T,n, Hvec, Δvec, priorN; ITER=ITER, α=α, β=β)
 
 ########## make dataframes for plotting
